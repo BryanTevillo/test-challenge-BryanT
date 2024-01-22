c20d9f541ab26d9da15839dd5d2b78787bae1bc6
BryanTevillo <tevillo24@gmail.com>
2024-01-22 16:46:48 -0600

    Se agregan los tipos de casos de prueba para calculator y se realizan pruebas exitosas

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,3 +1,4 @@
+const { default: expect } = require("expect");
 const calculator = require("../calculator");
 
 describe("Pruebas al programa calculator", () => {
@@ -9,15 +10,15 @@ describe("Pruebas al programa calculator", () => {
     expect(calculator.substract(10, 5)).toBe(5);
   });
 
-  test("multiplies a * b", () => {
-    //
+  test("multiplies a * b se ingresa 10,5 resultado 50", () => {
+    expect(calculator.multiply(10, 5)).toBe(50);
   });
 
-  test("divides a / b", () => {
-    // Consider decimals
+  test("divides a / b se ingresa 10,5 resultado 2", () => {
+    expect(calculator.divide(10, 5)).toBe(2);
   });
 
-  test("Raises to power a ^ b", () => {
-    //
+  test("Raises to power a ^ b se ingresa 2,3 resultado 8", () => {
+    expect(calculator.power(2, 3)).toBe(8);
   });
 });
