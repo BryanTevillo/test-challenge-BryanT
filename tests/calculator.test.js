e3a4f70d97252c52aee75a2b432de1acd28b30ff
BryanTevillo <tevillo24@gmail.com>
2024-01-22 16:29:15 -0600

    Se agregan diferentes casos de prueba para users y se realizan pruebas exitosas tambien se agrega pruebas para calculadora

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,23 +1,23 @@
-const { default: expect } = require("expect");
-
 const calculator = require("../calculator");
 
-test("adds a + b", () => {
-  //
-});
+describe("Pruebas al programa calculator", () => {
+  test("adds a + b  se ingresa 1,2 resultado 3", () => {
+    expect(calculator.sum(1, 2)).toBe(3);
+  });
 
-test("substracts a - b", () => {
-  //
-});
+  test("substracts a - b se ingresa 10,5 resultado 5", () => {
+    expect(calculator.substract(10, 5)).toBe(5);
+  });
 
-test("multiplies a * b", () => {
-  //
-});
+  test("multiplies a * b", () => {
+    //
+  });
 
-test("divides a / b", () => {
-  // Consider decimals
-});
+  test("divides a / b", () => {
+    // Consider decimals
+  });
 
-test("Raises to power a ^ b", () => {
-  //
+  test("Raises to power a ^ b", () => {
+    //
+  });
 });
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,9 +1,39 @@
 // You already have the methods imported
-const { default: expect } = require('expect');
-const {getUser, getUsers} = require('../users');
-
+const users = require("../data");
+const {
+  getUserById,
+  getUserByUsername,
+  validatePassword,
+} = require("../users");
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
 
 // READ documentation
+describe("USer functions test ", () => {
+  it("Should get the user by its id ", () => {
+    expect(getUserById(1)).toEqual({
+      id: 1,
+      username: "hpLover4",
+      email: "sirious90@gmail.com",
+      password: "hArrydotCom",
+    });
+  });
+
+  it("Should get the user by its username ", () => {
+    expect(getUserByUsername("hpLover4")).toEqual({
+      id: 1,
+      username: "hpLover4",
+      email: "sirious90@gmail.com",
+      password: "hArrydotCom",
+    });
+  });
+
+  it("Should get true validate user with id =1  and password = hArrydotCom", () => {
+    expect(validatePassword(1, "hArrydotCom")).toEqual(true);
+  });
+
+  it("Should get false validate user with id= 2 and password  = hArrydotCom incorrect  ", () => {
+    expect(validatePassword(2, "hArrydotCom")).toEqual(false);
+  });
+});
